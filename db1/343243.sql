45dbdb962b34558b4efe96c690bb196948dd44e0
Константин Косолапов <knets@yandex.ru>
2023-11-14 14:34:47 +0400

    wip

---
+++ db1/343243.sql
@@ -0,0 +1,4 @@
+--liquibase formatted sql logicalFilePath:main/other/account_not_checked2
+
+/*28.12.2022 12:00*/
+/*28.12.2022 16:00*/
---
+++ schema1/depend/db/234.sql
@@ -0,0 +1,4 @@
+--liquibase formatted sql logicalFilePath:main/schema/account_not_checked
+
+/*28.12.2022 15:00*/
+/*28.12.2022 16:00*/
